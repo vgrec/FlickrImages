b19f3a98faa77abccd1b0e823740b3877d23d1eb
vgrec <grec.vs@gmail.com>
2014-10-20 08:25:30 +0300

    added API key, and increased the number of items per page

--- app/src/main/java/com/vgrec/flickrimages/Constants.java
+++ app/src/main/java/com/vgrec/flickrimages/Constants.java
@@ -2,8 +2,8 @@ package com.vgrec.flickrimages;
 
 public class Constants {
     private static final String BASE_URL = "https://api.flickr.com/services/rest/";
-    private static final String API_KEY = "8c0a03e678f636644357f9ac9cf7e49f";
-    public static final String PHOTO_SEARCH_URL = BASE_URL + "?method=flickr.photos.search&api_key=" + API_KEY + "&format=json&nojsoncallback=1&tags=%s";
+    private static final String API_KEY = "97e9fc9835ab601cee0fe21fce7b11aa";
+    public static final String PHOTO_SEARCH_URL = BASE_URL + "?method=flickr.photos.search&api_key=" + API_KEY + "&format=json&nojsoncallback=1&per_page=400&tags=%s";
 
     // https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_{size}.jpg
     public static final String IMAGE_URL = "https://farm%s.staticflickr.com/%s/%s_%s_%s.jpg";
